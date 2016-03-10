cced1658783a66ba16a101c0665b33db8be60765
Bede <bede.overend@gmail.com>
2016-03-11 09:51:19 +1100

    refactor test file into helper methods, use promises and arrows for sanity

--- test/gulp-process-inline.spec.js
+++ test/gulp-process-inline.spec.js
@@ -1,54 +1,60 @@
-var assert = require('assert');
-var File = require('vinyl');
-var processInline = require('..')();
+'use strict';
 
-function extract(input, selector, done) {
-  var extractScript = processInline.extract('script'),
-      fakeFile = new File({
-        contents: new Buffer(input)
-      });
+const assert = require('assert');
+const File = require('vinyl');
+const processInline = require('..')();
 
-  extractScript.write(fakeFile);
+function extract(input, selector) {
+  return new Promise((resolve) => {
+    let extractScript = processInline.extract('script'),
+        fakeFile = new File({
+          contents: new Buffer(input)
+        });
 
-  extractScript.once('data', function(file) {
-    // check the contents
-    extracted = file.contents.toString();
-    done(file);
+    extractScript.write(fakeFile);
+
+    extractScript.once('data', (file) => {
+      resolve(file);
+    });
   });
 }
 
 function restore(file, done) {
-  var restore = processInline.restore();
+  return new Promise(resolve => {
+    let restore = processInline.restore();
 
-  restore.once('data', function(file) {
-    done(file);
+    restore.once('data', (file) => {
+      resolve(file);
+    });
+
+    restore.write(file);
   });
-
-  restore.write(file);
 }
 
-describe('gulp-process-inline', function() {
-  describe('extract and restore', function() {
-    var js = 'for(var o=0;o<r.length;o++) { // @foo bar }',
-        input = '<body>Hello<script>'+js+'</script></body>',
+describe('gulp-process-inline', () => {
+  describe('extract and restore', () => {
+    let js = 'for(var o=0;o<r.length;o++) { // @foo bar }',
+        input = `<body>Hello<script>${js}</script></body>`,
         extracted,
         restored;
 
-    beforeEach(function(done) {
-      extract(input, 'script', function(partial) {
-        extracted = partial.contents.toString();
-        restore(partial, function(file) {
-          restored = file.contents.toString();
-          done();
+    beforeEach(() => {
+      return extract(input, 'script')
+        .then(partial => {
+          extracted = partial.contents.toString();
+          return partial;
+        })
+        .then(restore)
+        .then(file => {
+          restored = file.contents.toString()
         });
-      });
     });
 
-    it('should have extracted the js script', function() {
+    it('should have extracted the js script', () => {
       assert.equal(extracted, js);
     });
 
-    it('should have restored back to the original input', function() {
+    it('should have restored back to the original input', () => {
       assert.equal(restored, input);
     });
   });
